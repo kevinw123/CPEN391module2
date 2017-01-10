4a3f0c70bea08e2ea71b04669070da0f532fe8b3
kevinw123 <kevinwong_895@hotmail.com>
2017-01-09 23:07:54 -0800

    tutorial1.6

---
+++ tutorial1.6/qsys_tutorial/software/lcd_demo/hello_world.c
@@ -0,0 +1,36 @@
+/*
+ * "Hello World" example.
+ *
+ * This example prints 'Hello from Nios II' to the STDOUT stream. It runs on
+ * the Nios II 'standard', 'full_featured', 'fast', and 'low_cost' example
+ * designs. It runs with or without the MicroC/OS-II RTOS and requires a STDOUT
+ * device in your system's hardware.
+ * The memory footprint of this hosted application is ~69 kbytes by default
+ * using the standard reference design.
+ *
+ * For a reduced footprint version of this template, and an explanation of how
+ * to reduce the memory footprint for a given application, see the
+ * "small_hello_world" template.
+ *
+ */
+
+#include "altera_up_avalon_character_lcd.h"
+
+int main(void)
+{
+	alt_up_character_lcd_dev * char_lcd_dev;
+	// open the Character LCD port
+	char_lcd_dev = alt_up_character_lcd_open_dev ("/dev/character_lcd_0");
+	if ( char_lcd_dev == NULL)
+	alt_printf ("Error: could not open character LCD device\n");
+	else
+	alt_printf ("Opened character LCD device\n");
+	/* Initialize the character display */
+	alt_up_character_lcd_init (char_lcd_dev);
+	/* Write "Welcome to" in the first row */
+	alt_up_character_lcd_string(char_lcd_dev, "Welcome to");
+	/* Write "the DE2 board" in the second row */
+	char second_row[] = "the DE2 board\0";
+	alt_up_character_lcd_set_cursor_pos(char_lcd_dev, 0, 1);
+	alt_up_character_lcd_string(char_lcd_dev, second_row);
+}
